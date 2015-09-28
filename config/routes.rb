86bbcea8e3361d1e1c6e51aba83a462abf63ea58
Christian Neal <c.neal91@gmail.com>
2015-09-27 22:30:11 -0500

    Add bootstrap and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
 
   get 'sign_in' => 'sessions#new', as: :sign_in
   post 'sign_in' => 'sessions#create'
+  delete 'sign_in' => 'sessions#delete', as: :sign_out
 
   get '/users/new' => 'users#new', as: :sign_up
   post '/users' => 'users#create', as: :users
 
-
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -64,4 +65,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
--- test/features/login_can_view_feed_test.rb
+++ test/features/login_can_view_feed_test.rb
@@ -1,6 +1,23 @@
 require "test_helper"
 
 class LoginCanViewFeedTest < Capybara::Rails::TestCase
+
+  test "Can sign out" do
+    me = User.create email: 'neal@example.com', password: '12345678'
+
+    visit root_path
+
+    fill_in "Email", with: 'neal@example.com'
+    fill_in "Password", with: '12345678'
+    click_button 'Sign In'
+
+    assert_content page, '#TheSecret'
+
+    click_link 'Sign Out'
+
+    refute_content page, '#TheSecret'
+  end
+
   test "Cannot view feed when not logged in" do
     me = User.create email: 'neal@example.com', password: '12345678'
 
@@ -18,6 +35,6 @@ class LoginCanViewFeedTest < Capybara::Rails::TestCase
     click_button 'Sign In'
 
     assert_content page, '#TheSecret'
-
   end
+
 end
